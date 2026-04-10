d90f00e905b79504af748b66a6c900a5a5aca10b
bella-ivanova <bibi123bon@gmail.com>
2026-04-10 17:44:26 +0300

    Small change to the initial system prompt

--- obj/Debug/net10.0/StudyAssist.AssemblyInfo.cs
+++ obj/Debug/net10.0/StudyAssist.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudyAssist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc6458ef79573bfb17b149104efb8022e229735")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b66164d384de4457f6702230581f4dd46a025272")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudyAssist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudyAssist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
